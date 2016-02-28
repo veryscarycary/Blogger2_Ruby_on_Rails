f734859ac5349741aac01414ea8cbbe52b1644a2
Cary Meskell <carymeskell@gmail.com>
2016-02-28 04:34:14 -0800

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tag = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id]).destroy
+
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    redirect_to '/tags.html' # redirect to index
+  end
+end
